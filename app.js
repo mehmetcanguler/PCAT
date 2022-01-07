e10be48171f923126757bf96b99b3f1d1fc17222
Mehmet Can GÜLER <91988777+mehmetcanguler@users.noreply.github.com>
2022-01-07 23:04:48 +0300

    second commit

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-const a = 'merhabalar';
-console.log(a);
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  const photo={
+    id:1,
+    name:"photo name",
+    description:"photo description"
+
+  }
+  res.send(photo)
+
+});
+
+app.listen(port, () => {
+  console.log(`sunucu ${port} port değerinde başlatıldı`);
+});
+
